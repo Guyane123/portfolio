b41bdf76d906a3d4f255f3e9ac203ae2d26157f5
Guyane <claretdamien@gmail.com>
2023-03-22 15:23:59 +0100

    Page contact

--- README.md
+++ README.md
@@ -1,24 +1,34 @@
 # My Portfolio
-Personnal portfolio
 
-## A faire :
-- La page A propos
-- La page éxperience
-- Les Trucs vectoriel là
-- plus responsive 
-- Les différents langages que je connais
-- la flèche pour aller en haut sur la page Accueil
-- footer
-- page contact
-	-> Nom Prenom email message
+Mon portfolio.
 
-## Fait
-- ~~Animation~~
-- ~~la flèche pour aller en haut sur la page Accueil~~
-- ~~Black mode~~
-- ~~Marquee~~
-- ~~message de présentation~~
-- ~~Introduction~~
-- ~~un logo web~~
-- ~~un titre~~
-- ~~Go Up~~
+## Mes objectifs
+
+-   faire un mini C.V pour des entreprises
+-   M'entraîner sur un projet complet
+-   Me former sur de nouvelles technologies
+
+### A faire :
+
+-   La page A propos
+-   Les Trucs vectoriel là
+-   plus responsive
+-   Les différents langages que je connais
+-   footer
+-   backend pour la page "contact" (récuperer les messages de la page contact pour l'envoyer sur un API)
+-   page contact
+-   une meilleur card dans la page contact
+-   des animations pour la page contact
+
+### Fait
+
+-   ~~Animation~~
+-   ~~la flèche pour aller en haut sur la page Accueil~~
+-   ~~Black mode~~
+-   ~~Marquee~~
+-   ~~message de présentation~~
+-   ~~Introduction~~
+-   ~~un logo web~~
+-   ~~un titre~~
+-   ~~Go Up~~
+-   ~~la flèche pour aller en haut sur la page Accueil~~
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,96 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "../styles/Contact.css";
 
 const About = () => {
-    return <div className="Contact modules">{/* <input type></input> */}</div>;
+    const [mousePos, setMousePos] = useState(0);
+    useEffect(() => {
+        const handleMouseMove = (e) => {
+            console.log(e);
+            if (
+                // e.clientX < 100 &&
+                // e.clientX > 300 &&
+                e.clientY > 140 &&
+                e.clientY < 490
+            ) {
+                setMousePos({ x: e.clientX, y: e.clientY });
+            }
+        };
+        document
+            .getElementById("card")
+            .addEventListener("mousemove", handleMouseMove);
+        return () => {
+            document
+                .getElementById("card")
+                .removeEventListener("mousemove", handleMouseMove);
+        };
+    }, []);
+    return (
+        <div className="Contact modules">
+            <div id="card" className="card">
+                <div
+                    id="circle"
+                    style={{
+                        left: (mousePos.x / window.innerWidth) * 100 + "%",
+                        top: (mousePos.y / window.innerHeight) * 100 + "%",
+                    }}
+                ></div>
+                <div className="block">
+                    <h3>
+                        ({mousePos.x}, {mousePos.y})
+                    </h3>
+                    <h3>Contact</h3>
+                    <a href="mailto:m.claretdamien@gmail">
+                        claretdamien@gmail.com
+                    </a>
+                    <p>0633191067</p>
+                    <h4>Bretagne, Morbihan</h4>
+                </div>
+            </div>
+            <div className="contactInputs">
+                <label for="name">
+                    <span>Votre prénom</span>
+                    <span className="required">*</span>
+                </label>
+                <input
+                    id="name"
+                    name="name"
+                    className="input"
+                    required
+                    placeholder="Your name"
+                    type="text"
+                ></input>
+                <label for="email">
+                    <span>E-mail</span>
+                    <span className="required">*</span>
+                </label>
+                <input
+                    id="email"
+                    name="email"
+                    required
+                    className="input"
+                    placeholder="email"
+                    type="email"
+                ></input>{" "}
+                <br />
+                <br />
+                <label for="message">
+                    <span>Message</span>
+                    <span className="required">*</span>
+                </label>
+                <input
+                    id="message"
+                    name="message"
+                    required
+                    className="input"
+                    placeholder="your text"
+                    type="text"
+                ></input>{" "}
+                <br />
+                <br />
+                <input id="submit" className="input" type="submit"></input>
+            </div>
+        </div>
+    );
 };
 
 export default About;
