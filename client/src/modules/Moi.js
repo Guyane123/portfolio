541d45eeaaf395fad14ec1738c71dd0f0bd994a7
Guyane <claretdamien@gmail.com>
2023-05-14 19:10:34 +0200

    littles improvements

--- client/src/modules/Moi.js
+++ client/src/modules/Moi.js
@@ -3,44 +3,13 @@ import React from "react";
 const Moi = () => {
     return (
         <div className="Moi">
-            <span id="r" className="letters">
-                R
-            </span>
-            <span id="e" className="letters">
-                e
-            </span>
-            <span id="g" className="letters">
-                g
-            </span>
-            <span id="a" className="letters">
-                a
-            </span>
-            <span id="r1" className="letters">
-                r
-            </span>
-            <span id="d" className="letters">
-                d
-            </span>
-            <span id="e1" className="letters">
-                e
-            </span>
-            <span id="z" className="letters">
-                z{" "}
-            </span>
-            <span id="m" className="letters">
-                m
-            </span>
-            <span id="o" className="letters">
-                o
-            </span>
-            <span id="n" c lassName="letters">
-                n
-            </span>
-            <span className="letters">
+            <div className="MoiContainer">
+                Regardez mon
                 <a target="blank" href="https://github.com/Guyane123">
-                    <span> </span>Github !
+                    {" "}
+                    Github !
                 </a>
-            </span>{" "}
+            </div>
         </div>
     );
 };
